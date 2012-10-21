f143ef8bf4af924e28087e537cca3f11b1d17877
Henry de Valence <git@hdevalence.ca>
2012-10-20 23:18:04 -0400

    Add index page.

--- site.hs
+++ site.hs
@@ -1,16 +1,25 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Control.Arrow ((>>>))
-import Data.Monoid (mempty)
+module Main where
+
+import Prelude hiding (id)
+import Control.Category (id)
+import Control.Arrow ((>>>), (***), arr)
+import Data.Monoid (mempty, mconcat)
+import Data.List.Split (splitEvery)
 
 import Hakyll
 
 main :: IO ()
 main = hakyll $ do
-    -- Twitter Bootstrap
+    -- CSS and Twitter Bootstrap
     match "css/bootstrap.css" $ route idRoute
     create "css/bootstrap.css" $ constA mempty
         >>> unixFilter "lessc" ["--compress", "bootstrap/less/bootstrap.less"]
+    match "css/*" $ do
+        route idRoute
+        compile copyFileCompiler
 
+    -- Copy images
     match "images/*" $ do
         route idRoute
         compile copyFileCompiler
@@ -18,6 +27,7 @@ main = hakyll $ do
     match "templates/*" $ do 
         compile templateCompiler
 
+    -- Blog posts
     match "blog/*" $ do
         route $ setExtension "html"
         compile $ pageCompiler
@@ -25,6 +35,25 @@ main = hakyll $ do
             >>> applyTemplateCompiler "templates/blog.hamlet"
             >>> relativizeUrlsCompiler
 
+    -- Create the blog index page
+    match "blog/index.html" $ route idRoute
+    create "blog/index.html" $ constA mempty
+        >>> arr (setField "title" "Henry de Valence :: Blog")
+        >>> requireAllA "blog/*.md" (id *** arr (reverse . sortByBaseName) >>> addPostList)
+        >>> applyTemplateCompiler "templates/postlist.hamlet"
+        >>> applyTemplateCompiler "templates/blog.hamlet"
+        >>> relativizeUrlsCompiler
 
+    -- Create the main page
+    match "index.html" $ route idRoute
+    create "index.html" $ constA mempty
+        >>> arr (setField "title" "Henry de Valence")
+        >>> applyTemplateCompiler "templates/index.hamlet"
+        >>> relativizeUrlsCompiler
 
+addPostList :: Compiler (Page String, [Page String]) (Page String)
+addPostList = setFieldA "list" $
+                    require "templates/post.hamlet" (\p t -> map (applyTemplate t) p)
+                >>> arr mconcat
+                >>> arr pageBody
 
