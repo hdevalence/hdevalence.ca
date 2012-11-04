52446c941415b7341553f47a6228a893f585514f
Henry de Valence <git@hdevalence.ca>
2012-11-04 00:30:50 -0400

    Try to guess mime types first.

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,11 @@
 
 # We sync blog entries first, so that we can add the content-type
 # header and omit .html extensions.
-echo Syncing html pages
+echo Syncing html pages from blog
 s3cmd sync _site/blog/ \
 	s3://www.hdevalence.ca/blog/ \
 	--add-header='Cache-Control':'public, max-age 2592000' \
-	--mime-type='text/html' \
+	--mime-type='text/html' -M \
 	--delete-removed
 
 echo Syncing other data
